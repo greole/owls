2fc4d1f2ccccb50f3f4c0ab70c45cc49a2a0ce20
Gregor <gregor.olenik@web.de>
2014-07-06 17:14:27 +0200

    FEAT implement __version__ string

--- FoamAna/__init__.py
+++ FoamAna/__init__.py
@@ -1,5 +1,6 @@
 #__all__ = ["case", "plot", "analysis"]
 
+from .version import __version__
 from analysis import *
 from plot import *
 from case import *
--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import setup
 
+package_name = 'FoamAna'
+# Get the version from FoamAna/version.py without importing the package
+exec(compile(open(package_name + '/version.py').read(),
+             package_name + '/version.py', 'exec'))
+
 config = {
     'author': 'Gregor Olenik',
     'author_email': 'itvgo@itv.uni-stuttgart.de',
     'description': 'A simple library for reading and processing OpenFOAM data',
     'license': 'BSD',
-    'version': '0.2.3',
-    'packages': ['FoamAna'],
+    'version': __version__,
+    'packages': [package_name],
     'install_requires': ['numpy',
                          'pandas',
                          'matplotlib',
