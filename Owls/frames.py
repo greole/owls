dddb994d528b5effe6023b524a1749e6c19565eb
gregor <you@example.com>
2015-01-13 10:31:10 +0100

    specify an order for multimerge plots

--- Owls/frames.py
+++ Owls/frames.py
@@ -67,25 +67,48 @@ def merge(*args, **kwargs):
     return bk.curplot()
 
 def multi_merge(*args, **kwargs):
-    """ call merge for all args """
+    """ call merge for all args
+
+        Examples:   mm=multi_merge(
+                        sets1.latest.by_index('Loc'),
+                        sets2.latest.by_index('Loc'),
+                        by='[0-9]+',
+                        x='Pos',
+                        y='vMean'
+                        order=[x-10,x+25])
+
+    """
     import bokeh.plotting as bk
     y = kwargs.get('y',None)
     x = kwargs.get('x','Pos')
     plots=[]
     c = args[0]
-    for name, data in c.iteritems():
+    # go through all items to be plotted
+    items = (
+        ((name,data) for name, data in c.iteritems() if name in kwargs['order'])
+        if kwargs.get('order',False) else c.iteritems()
+    )
+    for name, data in items:
         sub_plots=[data]
         colors = plt.next_color()
         for c_ in args[1:]:
+            # and through all sets to be plotted
             for name_, plot_ in c_.iteritems():
-                # now see if we have a match
-                selector = kwargs.get('by', True) #FIXME
-                if (not re.search(selector,name) or
-                    not re.search(selector,name_)):
-                    continue
-                if (re.search(selector,name).group()
-                    == re.search(selector,name_).group()):
-                    sub_plots.append(plot_)
+                if not kwargs.get('order', False):
+                    # select by regex
+                    # now see if we have a match
+                    selector = kwargs.get('by', "[A-Za-z0-9_\-]")
+                    # skip if search is empty 
+                    if not re.search(selector, name_):
+                        continue
+                    # append to subplot if same schema
+                    if (re.search(selector, name).group()
+                        == re.search(selector, name_).group()):
+                        sub_plots.append(plot_)
+                else:
+                    #select by name in order list
+                    if name_ == name:
+                        sub_plots.append(plot_)
         plots.append(merge(*sub_plots, x=x, y=y, title=name, colors=colors))
     return plots
 
