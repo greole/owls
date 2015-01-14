2a47bb9d64b98745d018b8b5ca24b5db5839d0b9
gregor <you@example.com>
2015-01-14 12:48:28 +0100

    create database if not present on import

--- Owls/__init__.py
+++ Owls/__init__.py
@@ -1,4 +1,9 @@
 #__all__ = ["case", "plot", "analysis"]
+import os
+
+owls_path = os.path.expanduser('~') + "/.owls"
+if not os.path.exists(owls_path):
+    os.makedirs(owls_path)
 
 from .version import __version__
 from io import *
