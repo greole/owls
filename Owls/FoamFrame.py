e0a6129aaaacc517df5a8067329e8851a5b445ca
gregor <gregor.olenik@web.de>
2015-02-18 15:23:20 +0100

    moved merge and multi merge to Owls.plot -> untested, Feat: read decomposed cases - untested

--- Owls/FoamFrame.py
+++ Owls/FoamFrame.py
@@ -26,18 +26,25 @@ def items_from_dict(dict, func, **kwargs):
 
 def read_sets(folder, name="None", search="(postProcessing)*sets/" + io.FPNUMBER, **kwargs):
     return FoamFrame(folder=folder, search_files=False,
-            search_pattern=search, name=name, show_func="plot", **kwargs)
+            search_pattern=search, name=name,
+            show_func="plot", preHooks=None, **kwargs)
 
 def read_lag(folder, files, skiplines=1,
-        name="None", cloud="[A-Za-z]*Cloud1", **kwargs
+        name="None", cloud="[A-Za-z]*Cloud1",
+        preHooks=None, decomposed=False, **kwargs
     ):
+    search = io.FPNUMBER + "/lagrangian/" + cloud,
+    search = (search if not decomposed else "processor[0-9]?/" + search)
     return FoamFrame(folder=folder, search_files=files,
-            search_pattern=io.FPNUMBER + "/lagrangian/{}".format(cloud),
-            name=name, skiplines=skiplines, show_func="scatter", **kwargs)
+                search_pattern=search, name=name, skiplines=skiplines,
+                show_func="scatter", **kwargs)
 
-def read_eul(folder, files, skiplines=1, name="None", preHooks=None, **kwargs):
+def read_eul(folder, files, skiplines=1, name="None", decomposed=False,
+            preHooks=None, **kwargs):
+    search = io.FPNUMBER
+    search = (search if not decomposed else "processor[0-9]?/" + search)
     return FoamFrame(folder=folder, search_files=files,
-            search_pattern=io.FPNUMBER, name=name,
+            search_pattern=search, name=name,
             skiplines=skiplines, show_func="scatter", preHooks=preHooks,
              **kwargs)
 
@@ -60,70 +67,6 @@ def read_log(folder, keys, log_name='*log*', plot_properties=False):
             show_func="plot",
             )
     return ff
-
-def merge(*args, **kwargs):
-    import bokeh.plotting as bk
-    bk.figure()
-    bk.hold()
-    y = kwargs.get('y',None)
-    x = kwargs.get('x','Pos')
-    try:
-        kwargs.pop('y')
-        kwargs.pop('x')
-    except:
-        pass
-    y = (y if type(y) == list else [y]*len(args)) #FIXME do the same for x
-    for yi,p in zip(y,args):
-        p.show(x=x, y=yi, color=next(kwargs["colors"]), **kwargs)
-    return bk.curplot()
-
-def multi_merge(*args, **kwargs):
-    """ call merge for all args
-
-        Examples:   mm=multi_merge(
-                        sets1.latest.by_index('Loc'),
-                        sets2.latest.by_index('Loc'),
-                        by='[0-9]+',
-                        x='Pos',
-                        y='vMean'
-                        order=[x-10,x+25])
-
-    """
-    import bokeh.plotting as bk
-    y = kwargs.get('y',None)
-    x = kwargs.get('x','Pos')
-    plots=[]
-    c = args[0]
-    # go through all items to be plotted
-    items = (
-        ((name,data) for name, data in c.iteritems() if name in kwargs['order'])
-        if kwargs.get('order',False) else c.iteritems()
-    )
-    for name, data in items:
-        sub_plots=[data]
-        colors = plt.next_color()
-        for c_ in args[1:]:
-            # and through all sets to be plotted
-            for name_, plot_ in c_.iteritems():
-                if not kwargs.get('order', False):
-                    # select by regex
-                    # now see if we have a match
-                    selector = kwargs.get('by', "[A-Za-z0-9_\-]")
-                    # skip if search is empty
-                    if (not re.search(selector, name) or
-                        not re.search(selector, name_)):
-                        continue
-                    # append to subplot if same schema
-                    if (re.search(selector, name).group()
-                        == re.search(selector, name_).group()):
-                        sub_plots.append(plot_)
-                else:
-                    #select by name in order list
-                    if name_ == name:
-                        sub_plots.append(plot_)
-        plots.append(merge(*sub_plots, x=x, y=y, title=name, colors=colors))
-    return plots
-
 
 class PlotProperties():
 
@@ -220,6 +163,8 @@ class FoamFrame(DataFrame):
 
       keys = [
           'skiplines',
+          'skiptimes',
+          'preHooks',
           'name',
           'symb',
           'search_files',
