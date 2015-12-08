1d425864c222e936fdb3197674ec6981b8fa2a40
gregor <gregor.olenik@web.de>
2015-12-08 22:57:06 +0100

    draft of debian install script

---
+++ scripts/install-debian.sh
@@ -0,0 +1,20 @@
+packages=(
+    python-matplotlib,
+    python-numpy
+    ipython-notebook
+)
+
+for package in "${packages[@]}"
+do
+        sudo apt-get install -y $package
+done
+
+inst () {
+    cd .. &&  python setup.py install --user
+}
+
+cur_dir=$( basename "$1" )
+if [ cur_dir == "scripts" ]; then
+    inst
+fi
+
