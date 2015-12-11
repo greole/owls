46d320e8eba84a58492bd25f8349da4998e7be3b
gregor <gregor.olenik@web.de>
2015-12-11 11:57:13 +0100

    removed deprecated merge function add legend style methods

--- Owls/plot.py
+++ Owls/plot.py
@@ -58,10 +58,6 @@ def adjustRow(style, whereRow, whereFigs=None, rows=None):
             setattr(f, key, value)
     return figs
 
-def figure():
-    import bokeh.plotting as bk
-    return bk.figure()
-
 def adjustColumn(style, whereRow, whereFigs=None, rows=None):
     for row in whereRow(rows):
         row = (whereFigs(row) if whereFigs else row)
@@ -80,97 +76,97 @@ def figure():
     return bk.figure()
 
 
-def merge(*args, **kwargs):
-    import bokeh.plotting as bk
-    figure = (bk.figure() if not kwargs.get('figure', False) else
-              kwargs.get('figure'))
-    y = kwargs.get('y', None)
-    x = kwargs.get('x', 'Pos')
-    try:
-        kwargs.pop('y')
-        kwargs.pop('x')
-    except:
-        pass
-    y = (y if type(y) == list else [y]*len(args))  # FIXME do the same for x
-    kwargs.pop('figure')
-    legend = kwargs.get('legend')
-    if legend:
-        kwargs.pop('legend')
-    for yi, p in zip(y, args):
-        if legend:
-            kwargs.update({'legend': p.properties.name})
-        override_color = p.properties.plot_properties.properties.get('Color', False)
-        color = (override_color if override_color else next(kwargs["colors"]))
-        p.show(x=x, y=yi, color=color, symbol=next(kwargs["symbols"]), figure=figure, **kwargs)
-    return figure
-
-
-def multi_merge(*args, **kwargs):
-    """ call merge for all args
-
-        Examples:   mm=multi_merge(
-                        sets1.latest.by_index('Loc'),
-                        sets2.latest.by_index('Loc'),
-                        by='[0-9]+',
-                        x='Pos',
-                        y='vMean'
-                        order=[x-10,x+25])
-
-    """
-    import bokeh.plotting as bk
-    y = kwargs.get('y', None)
-    x = kwargs.get('x', 'Pos')
-    if kwargs.get('legend'):
-        legend = kwargs.get('legend')
-        kwargs.pop('legend')
-    else:
-        legend = False
-        kwargs.pop('legend')
-    plots = []
-    c = args[0]
-    # go through all items to be plotted
-    items = (
-        ((name, data) for name, data in c.items() if name in kwargs['order'])
-        if kwargs.get('order', False) else c.items()
-    )
-    for nr, (name, data) in enumerate(items):
-        sub_plots = [data]
-        colors = next_color()
-        symbols = next_symbol()
-        figure = bk.figure()
-        for c_ in args[1:]:
-            # and through all sets to be plotted
-            for name_, plot_ in c_.items():
-                if not kwargs.get('order', False):
-                    # select by regex
-                    # now see if we have a match
-                    selector = kwargs.get('by', "[A-Za-z0-9_\-]")
-                    # skip if search is empty
-                    if (not re.search(selector, name) or
-                        not re.search(selector, name_)):
-                        continue
-                    # append to subplot if same schema
-                    if (re.search(selector, name).group()
-                        == re.search(selector, name_).group()):
-                        sub_plots.append(plot_)
-                else:
-                    # select by name in order list
-                    if name_ == name:
-                        sub_plots.append(plot_)
-        for kw in ['x', 'y']:
-            try:
-                kwargs.pop(kw)
-            except:
-                pass
-        if kwargs.get('legend_pos', 0) != nr:
-            legend = False
-        title = (kwargs.get('titles')[nr] if kwargs.get('titles', False) else name)
-        plots.append(merge(
-            *sub_plots, x=x, y=y,
-            title=title, colors=colors,
-            symbols=symbols, figure=figure,
-            legend=legend, **kwargs))
-    return plots
+# def merge(*args, **kwargs):
+#     import bokeh.plotting as bk
+#     figure = (bk.figure() if not kwargs.get('figure', False) else
+#               kwargs.get('figure'))
+#     y = kwargs.get('y', None)
+#     x = kwargs.get('x', 'Pos')
+#     try:
+#         kwargs.pop('y')
+#         kwargs.pop('x')
+#     except:
+#         pass
+#     y = (y if type(y) == list else [y]*len(args))  # FIXME do the same for x
+#     kwargs.pop('figure')
+#     legend = kwargs.get('legend')
+#     if legend:
+#         kwargs.pop('legend')
+#     for yi, p in zip(y, args):
+#         if legend:
+#             kwargs.update({'legend': p.properties.name})
+#         override_color = p.properties.plot_properties.properties.get('Color', False)
+#         color = (override_color if override_color else next(kwargs["colors"]))
+#         p.show(x=x, y=yi, color=color, symbol=next(kwargs["symbols"]), figure=figure, **kwargs)
+#     return figure
+#
+#
+# def multi_merge(*args, **kwargs):
+#     """ call merge for all args
+#
+#         Examples:   mm=multi_merge(
+#                         sets1.latest.by_index('Loc'),
+#                         sets2.latest.by_index('Loc'),
+#                         by='[0-9]+',
+#                         x='Pos',
+#                         y='vMean'
+#                         order=[x-10,x+25])
+#
+#     """
+#     import bokeh.plotting as bk
+#     y = kwargs.get('y', None)
+#     x = kwargs.get('x', 'Pos')
+#     if kwargs.get('legend'):
+#         legend = kwargs.get('legend')
+#         kwargs.pop('legend')
+#     else:
+#         legend = False
+#         kwargs.pop('legend')
+#     plots = []
+#     c = args[0]
+#     # go through all items to be plotted
+#     items = (
+#         ((name, data) for name, data in c.items() if name in kwargs['order'])
+#         if kwargs.get('order', False) else c.items()
+#     )
+#     for nr, (name, data) in enumerate(items):
+#         sub_plots = [data]
+#         colors = next_color()
+#         symbols = next_symbol()
+#         figure = bk.figure()
+#         for c_ in args[1:]:
+#             # and through all sets to be plotted
+#             for name_, plot_ in c_.items():
+#                 if not kwargs.get('order', False):
+#                     # select by regex
+#                     # now see if we have a match
+#                     selector = kwargs.get('by', "[A-Za-z0-9_\-]")
+#                     # skip if search is empty
+#                     if (not re.search(selector, name) or
+#                         not re.search(selector, name_)):
+#                         continue
+#                     # append to subplot if same schema
+#                     if (re.search(selector, name).group()
+#                         == re.search(selector, name_).group()):
+#                         sub_plots.append(plot_)
+#                 else:
+#                     # select by name in order list
+#                     if name_ == name:
+#                         sub_plots.append(plot_)
+#         for kw in ['x', 'y']:
+#             try:
+#                 kwargs.pop(kw)
+#             except:
+#                 pass
+#         if kwargs.get('legend_pos', 0) != nr:
+#             legend = False
+#         title = (kwargs.get('titles')[nr] if kwargs.get('titles', False) else name)
+#         plots.append(merge(
+#             *sub_plots, x=x, y=y,
+#             title=title, colors=colors,
+#             symbols=symbols, figure=figure,
+#             legend=legend, **kwargs))
+#     return plots
 
 
 def plot_cases(cases, y, order, x='Pos', legend=True, **kwargs):
@@ -202,7 +198,7 @@ from functional import compose
 rcParams = {
     'fig_size': {'min_border': 12,
                  'plot_height': 350,
-                 'plot_width': 900},
+                 'plot_width': 300},
     'font': {'axis.axis_label_text_font_size': "14pt",
              #'legend.label_text_font_size': "10pt",
              #'axis.major_label_text_font_style': 'bold',
@@ -226,6 +222,21 @@ color = partial(adjustColumn, rcParams['color'], all, all)
 cleanTitle = partial(adjustColumn, {'title': ""}, tail, all)
 cleanXAxis = partial(adjustColumn, {'xaxis.visible': False}, rtail, all)
 cleanYAxis = partial(adjustColumn, {'yaxis.visible': False}, all, tail)
+
+
+cleanLegendIa = partial(adjustColumn, {'legend.legends': []}, rtail, all)
+cleanLegendIb = partial(adjustColumn, {'legend.legends': []}, all, rtail)
+cleanLegendIIa = partial(adjustColumn, {'legend.border_line_color': None}, all, rtail)
+cleanLegendIIb = partial(adjustColumn, {'legend.border_line_color': None}, rtail, all)
+
+legendTopLeft = partial(adjustColumn, {'legend.orientation': "top_left"}, all, all)
+legendBottomLeft = partial(adjustColumn, {'legend.orientation': "bottom_left"}, all, all)
+legendTopRight = partial(adjustColumn, {'legend.orientation': "top_right"}, all, all)
+legendBottomRight = partial(adjustColumn, {'legend.orientation': "bottom_right"}, all, all)
+
+
+lastLegend = [cleanLegendIa, cleanLegendIb, cleanLegendIIa, cleanLegendIIb]
+
 
 arangement = lambda x: np.array(x).reshape(greatest_divisor(len(x)),-1).tolist()
 
